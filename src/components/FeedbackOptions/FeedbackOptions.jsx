96ddf3f74c2d0fe182843814f9aeb468e6fa150d
chekirko <victorche59@gmail.com>
2022-09-13 17:11:22 +0300

    додає логіку без стилів

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,23 @@
+// import PropTypes from 'prop-types';
+
+export const FeedbackOptions = ({ onLeaveFeedback }) => {
+  return (
+    <ul>
+      <li>
+        <button type="button" name="good" onClick={onLeaveFeedback}>
+          Good
+        </button>
+      </li>
+      <li>
+        <button type="button" name="neutral" onClick={onLeaveFeedback}>
+          Neutral
+        </button>
+      </li>
+      <li>
+        <button type="button" name="bad" onClick={onLeaveFeedback}>
+          Bad
+        </button>
+      </li>
+    </ul>
+  );
+};
